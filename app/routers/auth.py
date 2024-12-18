a6992ff680b305b7e165740714aa76a2f3702f18
CrisSaavedra <crisisaave@gmail.com>
2024-12-17 23:37:16 -0400

    Refactor registration endpoint by removing commented code to improve readability and maintain clarity in AuthRouter

--- app/routers/auth.py
+++ app/routers/auth.py
@@ -13,15 +13,7 @@ class User(BaseModel):
     
 #--------------------------
 
-
 AuthRouter = APIRouter()
-
-# @AuthRouter.post("/register")
-# async def register(new_user: User ,supabase: Client = Depends(get_supabase)):
-#     hashed_password = bcrypt.hashpw(new_user.password_hash.encode('utf-8'), bcrypt.gensalt())
-#     new_user.password_hash = hashed_password.decode('utf-8')
-#     response = supabase.table('users').insert(new_user.model_dump()).execute()
-#     return response
 
 @AuthRouter.post("/register")
 async def register(new_user: User, supabase: Client = Depends(get_supabase)):
